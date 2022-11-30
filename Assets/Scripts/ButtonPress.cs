81fd5b8d8149d6b258ab392d4b913ae1d5107336
littlekillerkalf <thomaskalf@gmail.com>
2022-11-30 18:24:18 +1100

    importante

--- Assets/Scripts/ButtonPress.cs
+++ Assets/Scripts/ButtonPress.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class ButtonPress : MonoBehaviour
 {
+    public bool isButton = true;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "PresentCube")
+        if(other.tag == "PresentCube" && isButton)
         {
             Debug.Log("Fuck");
         }
-        if (other.tag == "PastCube")
+        if (other.tag == "PastCube" && isButton)
+        {
+            Debug.Log("Fuck");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "PresentCube" && isButton)
+        {
+            Debug.Log("Fuck");
+        }
+        if (other.tag == "PastCube" && isButton)
         {
             Debug.Log("Fuck");
         }
